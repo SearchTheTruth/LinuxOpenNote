799e035757ca1c936373a662c320e0a8985eb022
Brain <jiangh_work@163.com>
2019-02-20 21:14:24 +0800

    [C++][Add]---Chapter6 默认实参

---
+++ c++/Chapter_6/6_42.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+string make_plural(size_t ctr, const string &word, const string &ending = "s")
+{
+    return (ctr > 1) ? word + ending : word;
+}
+
+int main()
+{
+    cout << make_plural(1, "success", "es") << " " << make_plural(1, "success") <<endl;
+    cout << make_plural(2, "failure", "es") << " " << make_plural(2, "failure") <<endl;
+
+    return 0;
+}
+
