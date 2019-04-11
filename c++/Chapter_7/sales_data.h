05d43901f64f3930493e90dfc5792888e83f5907
Brain <jiangh_work@163.com>
2019-04-11 19:27:50 +0800

    [C++][Add]---Chapter7 使用友元重新定义sales_data

--- c++/Chapter_7/sales_data.h
+++ c++/Chapter_7/sales_data.h
@@ -2,17 +2,22 @@
 #define _SALES_DATA_H
 #include <string>
 #include <iostream>
+
+
 struct sales_data {
+friend std::ostream &print(std::ostream &os, const struct sales_data &book);
+public:
     sales_data() = default;
     sales_data(const std::string &name): bookNo(name) {};
     sales_data(const std::string &name, unsigned num, double unitprice):
         bookNo(name), units_sold(num), revenue(unitprice * units_sold) {};
     sales_data(std::istream &is) {is >> bookNo >> units_sold >> revenue;};
+    std::string isbn() const {return bookNo;};
+    struct sales_data &combine (const struct sales_data &sec);
+private:
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0;
-    std::string isbn() const {return bookNo;};
-    struct sales_data &combine (const struct sales_data &sec);
 };
 
 inline std::ostream &print(std::ostream &os, const struct sales_data &book)
@@ -20,5 +25,4 @@ inline std::ostream &print(std::ostream &os, const struct sales_data &book)
     os << book.isbn() << " units: " << book.units_sold <<  " total: " << book.revenue << "$";
     return os;
 }
-
 #endif
