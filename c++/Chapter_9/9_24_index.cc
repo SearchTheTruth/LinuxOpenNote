44b06bef9027c4602b56f362343c698a0e74ce63
Brain <jiangh_work@163.com>
2019-08-11 17:31:11 +0800

    [C++][Add]---Chapter9 随即访问顺序容器的各种方法(at可以检查越界)

---
+++ c++/Chapter_9/9_24_index.cc
@@ -0,0 +1,12 @@
+#include <vector>
+using std::vector;
+
+#include <iostream>
+using std::cout; using std::cin; using std::endl;
+
+int main()
+{
+    vector<int> empty;
+    cout << empty[0];
+    return 0;
+}
