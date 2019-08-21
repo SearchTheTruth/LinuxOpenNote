7c1f37978bd8a1bc5ab3e48083b2c2457d3c3329
Brain <jiangh_work@163.com>
2019-08-21 15:57:02 +0800

    [C++][Add]---Chapter9 对list的insert,erase操作

---
+++ c++/Chapter_9/9_31.cc
@@ -0,0 +1,33 @@
+#include <list>
+using std::list;
+
+#include <iostream>
+using std::cout; using std::cin; using std::endl;
+
+void print(const auto &src)
+{
+    cout << "event num =" << src.size() << "=" << endl;
+    for (auto beg = src.begin(); beg != src.end(); ++beg)
+    {
+        cout << *beg << " ";
+    }
+    cout << endl << "++++++++++++++" << endl;
+}
+
+int main()
+{
+    list<int> mylist {0, 1, 2, 3, 4, 5, 6, 7, 9};
+    print(mylist);
+    auto cur = mylist.begin();
+    while (cur != mylist.end()) {
+        if (*cur & 0x1 == 0x1) {
+            cur = mylist.insert(cur, *cur);
+            ++cur;
+            ++cur;
+        } else {
+            cur = mylist.erase(cur);
+        }
+    }
+    print(mylist);
+    return 0;
+}
