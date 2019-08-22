2d0e777b36c8e7e104a34addcfed3294bc6406b4
Brain <jiangh_work@163.com>
2019-08-22 16:21:39 +0800

    [C++][Add]---Chapter9 9_43 使用下标和replace的版本

---
+++ c++/Chapter_9/9_44_v1.cc
@@ -0,0 +1,29 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout; using std::cin; using std::endl;
+
+void replace(string &s, const string &oldVal, const string &newVal)
+{
+    unsigned int idx = 0;
+    while (idx <= s.size() - oldVal.size()) {
+        if (s[idx] == oldVal[0] && s.substr(idx, oldVal.size()) == oldVal) {
+            s.replace(idx, oldVal.size(), newVal);
+            idx += newVal.size();
+        } else {
+            ++idx;
+        }
+    }
+}
+
+int main()
+{
+    string s("hello");
+    cout << s << endl;
+    replace(s, "lo", "ll");
+    cout << s << endl;
+    replace(s, "el", "ll");
+    cout << s << endl;
+    return 0;
+}
