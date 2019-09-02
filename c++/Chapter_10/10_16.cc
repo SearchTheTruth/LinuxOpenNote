9333b120a68457bdcedc248cd9b7ed7070b0c00d
Brain <jiangh_work@163.com>
2019-09-02 20:14:40 +0800

    [C++][Add]---Chapter10 使用lambda查找string序列中大于任意值的字符串 10_16.cc find_if版本 10_18.cc partition版本

---
+++ c++/Chapter_10/10_16.cc
@@ -0,0 +1,37 @@
+#include <algorithm>
+using std::find_if; using std::sort; using std::stable_sort; using std::for_each;
+
+#include <vector>
+using std::vector;
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout; using std::cin; using std::endl;
+
+void print(const string &s)
+{
+    cout << s << " ";
+}
+
+void biggest(vector<string> org, string::size_type sz)
+{
+    if (org.empty())
+        return;
+    sort(org.begin(), org.end());
+    stable_sort(org.begin(), org.end(), [](const string &s1, const string &s2)
+            {return s1.size() < s2.size();});
+    auto big = find_if(org.begin(), org.end(), [sz](const string &s)
+            {return s.size() >= sz;});
+    cout << org.end() - big << " words is longer than " << sz <<" character" << endl;
+    for_each(big, org.end(), print);
+}
+
+int main()
+{
+    vector<string> tmp{"hello", "my", "world", "the", "and", "but", "open", "and", "there", "apple", "my", "mouth"};
+    biggest(tmp, 5);
+    cout << endl;
+    return 0;
+}
