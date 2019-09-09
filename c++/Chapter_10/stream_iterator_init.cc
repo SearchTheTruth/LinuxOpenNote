8f94d90317638a0c3d5d390cb58bf6ae0720b9d8
jianghao <jianghao7@xiaomi.com>
2019-09-09 18:06:38 +0800

    [C++][Add]---Chapter10 流迭代器的典型使用(懒惰求值)
    Signed-off-by: jianghao <jianghao7@xiaomi.com>

---
+++ c++/Chapter_10/stream_iterator_init.cc
@@ -0,0 +1,22 @@
+#include <iterator>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+    istream_iterator<int> in(cin);
+    istream_iterator<int> eof;
+
+    vector<int> exp(in, eof);
+
+
+    cout << "print vector" << endl;
+    for (const auto &num : exp)
+        cout << num << " ";
+    cout << endl;
+    return 0;
+}
+
