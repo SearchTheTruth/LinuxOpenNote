213e403264df168ca1ef29bdb3fc969fced1bbdf
JJ <jiangh_work@163.com>
2019-10-28 08:47:34 -0700

    [C++][Add]---Chapter13 为HasPtr类增加析构函数

---
+++ c++/Chapter_13/13_11.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class HasPts {
+    friend void print(const HasPts &input);
+    public:
+    ~HasPts() {
+        delete ps;
+    }
+    HasPts& operator=(const HasPts& org) {
+        if (!ps)
+            delete ps;
+        ps = new std::string(*(org.ps));
+        i = org.i;
+        return *this;
+    }
+    HasPts(const std::string &s) : ps(new std::string(s)), i(0) {};
+    HasPts(const HasPts &org) : ps(new std::string(*(org.ps))), i(org.i) {};
+    private:
+        std::string *ps;
+        int i;
+};
+
+void print(const HasPts &input)
+{
+    std::cout << "ad: " << input.ps << " cnt: " << *(input.ps) << std::endl;
+}
+
+int main()
+{
+    HasPts org("org");
+    print(org);
+    HasPts dst(org);
+    print(dst);
+    HasPts equal("equal");
+    print(equal);
+    equal = org;
+    print(equal);
+    return 0;
+}
